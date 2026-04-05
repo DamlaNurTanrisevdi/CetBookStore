efcbf859e1fc780a92b81183cfcf8cab502bc617
damlanurtanrisevdi <dntnrsvd@gmail.com>
2026-04-05 22:11:44 +0300

    Remove old CetBookStore folder and solution file

--- CetBookStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CetBookStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
